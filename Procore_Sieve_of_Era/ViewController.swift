68e5a586e99d10dd342f22a1cb5a75a3b18ca9ab
Abdul Raqeeb <abdulraqeeb@abduls-macbook-pro.local>
2016-12-07 11:34:04 -0600

    Initial Commit

---
+++ Procore_Sieve_of_Era/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Procore_Sieve_of_Era
+//
+//  Created by Abdul Raqeeb on 12/7/16.
+//  Copyright © 2016 Abdul Raqeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
